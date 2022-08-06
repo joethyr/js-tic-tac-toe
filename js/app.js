926cfbf370fabf3b59fb668127934d9add36e2cf
joethyr <joethayr@gmail.com>
2022-08-05 20:48:26 -0600

    add factory fucntions to create Player objects

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 // put gameBoard in module
-const gameBoard = () => {
-  const board = []
-  return {
-    board
-  }
+const gameBoard = (() => {
+  const spaces = Array.from(document.querySelectorAll('.space'))
+  let restartBtn = document.getElementById('restartBtn')
+  const playerDisplay = document.querySelector('.display-player')
+  const winPrompt = document.getElementById('win-prompt')
+  
+  let board = ['', '', '', '', '', '', '', ''];
+
+  // restartBtn.addEventListener('click', restartBoard);
+})()
+
+// Create factory function for Player Object
+const Player = marker => {
+  const getMarker = () => marker
+  const moves = []
+  return { getMarker, moves}
 }
 
-// factory for creating players
-const Player = (name) => {
-  const getName = () => name
-  return {
-    getName
-  }
-}
+const playerOne = Player('x')
+const playerTwo = Player('o')
+
+console.log(playerOne.getMarker())
